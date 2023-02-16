2ab1a220e02e372d81fc8f4ce4abff36872243d2
ganz <g_anz@mail.ru>
2023-02-16 21:37:01 +0300

    added server ans sockets

---
+++ server.js
@@ -0,0 +1,21 @@
+// Зависимости
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/', express.static(__dirname + '/'));
+
+// Маршруты
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Запуск сервера
+server.listen(5000, function () {
+    console.log('Запускаю сервер на порте 5000');
+});
