4d65f56c268f610a79ee5f3ede7ceceeb113613a
g_anz <g_anz@mail.ru>
2023-05-03 12:17:42 +0300

    for linux

--- backend/httpserver.ts
+++ backend/httpserver.ts
@@ -4,11 +4,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 
-// const __filename = fileURLToPath(import.meta.url);
-const __filename = fileURLToPath("file:///D:/dev/canvas-game/canvas-game-bootstrap/dist/backend/httpserver.js");
-let __dirname = path.dirname(__filename);
-__dirname = __dirname.split("\\").slice(0,4).join("\\")
-
+const __filename = fileURLToPath(import.meta.url);
+let __dirname = path.resolve(path.dirname(__filename), "../..");
 
 var app = express();
 app.set('port', 5000);
